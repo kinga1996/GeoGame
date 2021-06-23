f702b2be338268eedd0005d76b5316b661628255
Kinga Kalisz <kingakalisz@gmail.com>
2021-06-23 20:53:06 +0200

    get data - done

--- app/src/main/java/com/example/geogame/MainActivity.java
+++ app/src/main/java/com/example/geogame/MainActivity.java
@@ -6,15 +6,19 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
-import android.database.sqlite.SQLiteDatabase;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.Random;
 
 
 public class MainActivity extends AppCompatActivity {
 
-    SQLiteDatabase sqldb=null;
     String Question,CorrectCountry,FalseQuestion1,FalseQuestion2,FalseQuestion3,trueAnswer;
     Random rand;
     Button startb,button1,button2,button3,nextb;
@@ -29,6 +33,10 @@ public class MainActivity extends AppCompatActivity {
         rand =new Random(System.currentTimeMillis());
         getViewsReferences();
         enableButtons(false);
+        String [] file1 = read1();
+        String [] file2 = read2();
+        question.setText(file1[0]);
+        counter.setText(file2[0]);
     }
 
     private void  getViewsReferences(){
@@ -46,6 +54,50 @@ public class MainActivity extends AppCompatActivity {
         button2.setEnabled(enable);
         button3.setEnabled(enable);
         nextb.setEnabled(enable);
+    }
+
+    public String[] read1() {
+        String data = "";
+        StringBuffer sbuffer = new StringBuffer();
+        InputStream is = this.getResources().openRawResource(R.raw.data1);
+        BufferedReader reader = new BufferedReader(new InputStreamReader(is));
+        String[] array = new String[0];
+        if(is!=null){
+            try{
+                while((data = reader.readLine()) != null){
+                    sbuffer.append((data + "///"));
+                }
+                String data1 = sbuffer.toString();
+                array = data1.split("///");
+                is.close();
+            }
+            catch(IOException e) {
+                e.printStackTrace();
+            }
+        }
+        return array;
+    }
+
+    public String[] read2() {
+        String data = "";
+        StringBuffer sbuffer = new StringBuffer();
+        InputStream is = this.getResources().openRawResource(R.raw.data2);
+        BufferedReader reader = new BufferedReader(new InputStreamReader(is));
+        String[] array = new String[0];
+        if(is!=null){
+            try{
+                while((data = reader.readLine()) != null){
+                    sbuffer.append((data + "///"));
+                }
+                String data1 = sbuffer.toString();
+                array = data1.split("///");
+                is.close();
+            }
+            catch(IOException e) {
+                e.printStackTrace();
+            }
+        }
+        return array;
     }
 
     
