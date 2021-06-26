573679c21a618f6dc4c6fccc6188d4dc86515df0
Kinga Kalisz <kingakalisz@gmail.com>
2021-06-26 21:31:48 +0200

    questions done

--- app/src/main/java/com/example/geogame/MainActivity.java
+++ app/src/main/java/com/example/geogame/MainActivity.java
@@ -19,41 +19,33 @@ import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
-    String Question,CorrectCountry,FalseQuestion1,FalseQuestion2,FalseQuestion3,trueAnswer;
-    Random rand;
-    Button startb,button1,button2,button3,nextb;
-    TextView question, counter;
-    Integer count=0;
-    ArrayList<String> questionsList = new ArrayList<>();
+    Button button1,button2,button3,button4,nextb,backb;
+    TextView question;
+    Random random = new Random();
+    int value = 0;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        rand =new Random(System.currentTimeMillis());
         getViewsReferences();
-        enableButtons(false);
-        String [] file1 = read1();
-        String [] file2 = read2();
-        question.setText(file1[0]);
-        counter.setText(file2[0]);
+        String[] file1 = read1();
+        String[] file2 = read2();
+        value = nextQuestion(file1, file2);
+    }
+
+    public void next(View v, String[] file1, String[] file2) {
+        value = nextQuestion(file1, file2);
     }
 
     private void  getViewsReferences(){
         button1 = findViewById(R.id.button1);
         button2 = findViewById(R.id.button2);
         button3 = findViewById(R.id.button3);
-        startb=findViewById(R.id.startb);
+        button4 = findViewById(R.id.button4);
         nextb=findViewById(R.id.nextb);
-        counter=findViewById(R.id.counter);
+        backb=findViewById(R.id.backb);
         question=findViewById(R.id.question);
-    }
-
-    private void enableButtons(boolean enable) {
-        button1.setEnabled(enable);
-        button2.setEnabled(enable);
-        button3.setEnabled(enable);
-        nextb.setEnabled(enable);
     }
 
     public String[] read1() {
@@ -98,6 +90,121 @@ public class MainActivity extends AppCompatActivity {
             }
         }
         return array;
+    }
+
+    public int nextQuestion(String[] file1, String[] file2){
+        int lfile1 = file1.length; //CONTINENT, COORDINATES
+        int lfile2 = file2.length; //COUNTRY, CAPITAL, POPULATION, CURRENCY
+        int randomCorrect =  random.nextInt((lfile2-1) - 0) + 0;
+        int randomButton =  random.nextInt(3 - 0) + 0;
+        int randomType =  random.nextInt(5 - 0) + 0; //CAPITAL, POPULATION, CURRENCY, CONTINENT, COORDINATES
+        int randomFalse[] = new int[3]; //zle
+        do {
+            randomFalse[0] = random.nextInt((lfile2 - 1) - 0) + 0;
+        } while (randomFalse[0] == randomCorrect);
+        do {
+            randomFalse[1] = random.nextInt((lfile2 - 1) - 0) + 0;
+        } while (randomFalse[1] == randomCorrect || randomFalse[1] == randomFalse[0] );
+        do {
+            randomFalse[2] = random.nextInt((lfile2 - 1) - 0) + 0;
+        } while (randomFalse[2] == randomCorrect || randomFalse[2] == randomFalse[1] || randomFalse[2] == randomFalse[0] );
+        String[] correct_file2 = file2[randomCorrect].split(":");
+        String[] false1_file2 = file2[randomFalse[0]].split(":");
+        String[] false2_file2 = file2[randomFalse[1]].split(":");
+        String[] false3_file2 = file2[randomFalse[2]].split(":");
+        String[] correct_file1 = new String[0];
+        String[] false1_file1 = new String[0];
+        String[] false2_file1 = new String[0];
+        String[] false3_file1 = new String[0];
+        for (int i = 0; i < lfile1; i++) {
+            if (file1[i].contains(correct_file2[0])) {
+                correct_file1 = file1[i].split(":");
+                break;
+            }
+        }
+        for (int i = 0; i < lfile1; i++) {
+            if (file1[i].contains(false1_file2[0])) {
+                false1_file1 = file1[i].split(":");
+                break;
+            }
+        }
+        for (int i = 0; i < lfile1; i++) {
+            if (file1[i].contains(false2_file2[0])) {
+                false2_file1 = file1[i].split(":");
+                break;
+            }
+        }
+        for (int i = 0; i < lfile1; i++) {
+            if (file1[i].contains(false3_file2[0])) {
+                false3_file1 = file1[i].split(":");
+                break;
+            }
+        }
+        String AnswerCorrect = "";
+        String AnswerFalse1 = "";
+        String AnswerFalse2 = "";
+        String AnswerFalse3 = "";
+        if (randomType == 0){ //CAPITOL
+            question.setText("What is the capital of " + correct_file2[0] + "?");
+            AnswerCorrect = correct_file2[1];
+            AnswerFalse1 = false1_file2[1];
+            AnswerFalse2 = false2_file2[1];
+            AnswerFalse3 = false3_file2[1];
+        }
+        if (randomType == 1){ //POPULATION
+            question.setText("What is the population of " + correct_file2[0] + "?");
+            AnswerCorrect = correct_file2[2];
+            AnswerFalse1 = false1_file2[2];
+            AnswerFalse2 = false2_file2[2];
+            AnswerFalse3 = false3_file2[2];
+        }
+        if (randomType == 2){ //CURRENCY
+            question.setText("What is the currency of " + correct_file2[0] + "?");
+            AnswerCorrect = correct_file2[3];
+            AnswerFalse1 = false1_file2[3];
+            AnswerFalse2 = false2_file2[3];
+            AnswerFalse3 = false3_file2[3];
+        }
+        if (randomType == 3){ //CONTINENT
+            question.setText("On which continent is " + correct_file2[0] + " located?");
+            AnswerCorrect = correct_file1[4];
+            AnswerFalse1 = false1_file1[4];
+            AnswerFalse2 = false2_file1[4];
+            AnswerFalse3 = false3_file1[4];
+
+        }
+        if (randomType == 4){ //COORDINATES
+            question.setText("What are the geographical coordinates of " + correct_file2[0] + "?");
+            AnswerCorrect = correct_file1[2] + " " + correct_file1[3];
+            AnswerFalse1 = false1_file1[2] + " " + false1_file1[3];
+            AnswerFalse2 = false2_file1[2] + " " + false1_file1[3];
+            AnswerFalse3 = false3_file1[2] + " " + false1_file1[3];
+        }
+        if(randomButton == 0){
+            button1.setText(AnswerCorrect);
+            button2.setText(AnswerFalse1);
+            button3.setText(AnswerFalse2);
+            button4.setText(AnswerFalse3);
+        }
+        if(randomButton == 1){
+            button2.setText(AnswerCorrect);
+            button1.setText(AnswerFalse1);
+            button3.setText(AnswerFalse2);
+            button4.setText(AnswerFalse3);
+        }
+        if(randomButton == 2){
+            button3.setText(AnswerCorrect);
+            button2.setText(AnswerFalse1);
+            button1.setText(AnswerFalse2);
+            button4.setText(AnswerFalse3);
+        }
+        if(randomButton == 3){
+            button4.setText(AnswerCorrect);
+            button2.setText(AnswerFalse1);
+            button3.setText(AnswerFalse2);
+            button1.setText(AnswerFalse3);
+        }
+        return randomButton;
     }
 
     
