3eb6b6239d653879d674966a2f3f069feba2179d
Kinga Kalisz <kingakalisz@gmail.com>
2021-06-28 20:37:22 +0200

    fina;

--- app/src/main/java/com/example/geogame/ParametersActivity.java
+++ app/src/main/java/com/example/geogame/ParametersActivity.java
@@ -12,9 +12,9 @@ import android.widget.TextView;
 public class ParametersActivity extends AppCompatActivity {
 
     static final String r_username = "player";
-    //static final String r_content = "1";
-    //String content;
     String username;
+    static final String r_points = "0";
+    String points = "0";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,24 +22,29 @@ public class ParametersActivity extends AppCompatActivity {
         setContentView(R.layout.activity_parameters);
         SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this);
         username = prefs.getString(r_username, "player");
-        //content = prefs.getString(r_content, "1");
+        points = prefs.getString(r_points, "0");
         TextView t_current_username = (TextView) findViewById(R.id.current_username);
         t_current_username.setText("Current username: " + username);
+        TextView t_current_points = (TextView) findViewById(R.id.current_points);
+        t_current_points.setText("Your points: " + points);
     }
 
     public void back(View v){
-        SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this);
-        SharedPreferences.Editor ed = prefs.edit();
-        //ed.putString(r_content, content);
-        ed.putString(r_username, username);
-        ed.commit();
         this.finish();
     }
 
     public void confirm_username(View v){
         EditText e_current_username = findViewById(R.id.edit_username);
         TextView t_current_username = (TextView) findViewById(R.id.current_username);
+        TextView t_current_points = (TextView) findViewById(R.id.current_points);
         username = e_current_username.getText().toString();
-        t_current_username.setText("Current usernme: "+username);
+        t_current_username.setText("Current username: "+username);
+        String points = "0";
+        t_current_points.setText("Your points: " + points);
+        SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this);
+        SharedPreferences.Editor ed = prefs.edit();
+        ed.putString(r_username, username);
+        ed.putString(r_points, "0");
+        ed.commit();
     }
 }
